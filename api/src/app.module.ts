9313ef1f7feeaa7b439897c7acc1326eeedb1a04
ythosa <vasus714@yandex.ru>
2020-12-12 14:05:08 +0300

    Close #4

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,15 +5,21 @@ import { LessonModule } from './lesson/lesson.module';
 import { Lesson } from './lesson/entities/lesson.entity';
 import { StudentModule } from './student/student.module';
 import { Student } from './student/entities/student.entity';
+import { configModule } from './configure.root';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
     imports: [
-        TypeOrmModule.forRoot({
-            type: 'mongodb',
-            url: 'mongodb://api-database/school',
-            synchronize: true,
-            useUnifiedTopology: true,
-            entities: [Lesson, Student],
+        configModule,
+        TypeOrmModule.forRootAsync({
+            useFactory: (configService: ConfigService) => ({
+                type: 'mongodb',
+                url: configService.get<string>('MONGODB_CONNECTION_URL'),
+                synchronize: true,
+                useUnifiedTopology: true,
+                entities: [Lesson, Student],
+            }),
+            inject: [ConfigService], 
         }),
         GraphQLModule.forRoot({
             autoSchemaFile: true,
